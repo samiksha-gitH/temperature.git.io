41f9b9e16e05fc5ee9f11b58d293cf5f71fe70cd
samiksha-gitH <samikshasr1704@gmail.com>
2023-09-15 19:26:58 +0530

    changed

---
+++ script.js
@@ -0,0 +1,25 @@
+let celsius = document.getElementById('celsius');
+let fahrenheit = document.getElementById('fahrenheit');
+let kelvin = document.getElementById('kelvin');
+
+celsius.oninput = function () {
+    let f = (parseFloat(celsius.value) * 9) / 5 + 32;
+    fahrenheit.value = parseFloat(f.toFixed(3));
+
+    let k = (parseFloat(celsius.value) + 273.15);
+    kelvin.value = parseFloat(k.toFixed(3));
+}
+fahrenheit.oninput = function () {
+    let c = ((parseFloat(fahrenheit.value) - 32) * 5) / 9;
+    celsius.value = parseFloat(c.toFixed(3));
+
+    let k = (parseFloat(fahrenheit.value) - 32) * 5 / 9 + 273.15;
+    kelvin.value = parseFloat(k.toFixed(3));
+}
+kelvin.oninput = function () {
+    let f = (parseFloat(kelvin.value) - 273.15) * 9 / 5 + 32;
+    fahrenheit.value = parseFloat(f.toFixed(3));
+
+    let c = (parseFloat(kelvin.value) - 273.15);
+    celsius.value = parseFloat(c.toFixed(3));
+}
